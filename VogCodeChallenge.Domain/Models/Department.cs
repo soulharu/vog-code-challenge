eb19260f274e733fdcfdc9380b6f13e263bff4b1
Sergio B. Ramos <sbramosj@gmail.com>
2022-01-15 18:33:18 -0300

    Add model classes

---
+++ VogCodeChallenge.Domain/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace VogCodeChallenge.Domain.Models
+{
+    public class Department
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ VogCodeChallenge.Domain/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿namespace VogCodeChallenge.Domain.Models
+{
+    public class Employee
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string JobTitle { get; set; }
+        public string MailingAddress { get; set; }
+        public long DepartmentId { get; set; }
+
+        public string FullName {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            } 
+        }
+    }
+}
