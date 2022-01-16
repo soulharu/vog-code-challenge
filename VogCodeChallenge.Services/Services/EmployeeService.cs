7f7ad7968deb10f52d5e30342379669ba98a287e
Sergio B. Ramos <sbramosj@gmail.com>
2022-01-16 15:35:25 -0300

    Implement employee services

--- VogCodeChallenge.Services/Services/EmployeeService.cs
+++ VogCodeChallenge.Services/Services/EmployeeService.cs
@@ -1,20 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using VogCodeChallenge.Domain.Models;
+using VogCodeChallenge.Infrastructure;
 using VogCodeChallenge.Services.Services.Interfaces;
 
 namespace VogCodeChallenge.Services.Services
 {
     public class EmployeeService : IEmployeeService
     {
+        private readonly DataSupplier _dataSupplier;
+
+        public EmployeeService()
+        {
+            _dataSupplier = new DataSupplier();
+        }
+
         public IEnumerable<Employee> GetAll()
         {
-            throw new NotImplementedException();
+            return _dataSupplier.GetEmployees();
         }
 
         public IList<Employee> ListAll()
         {
-            throw new NotImplementedException();
+            return _dataSupplier.GetEmployees();
         }
     }
 }
