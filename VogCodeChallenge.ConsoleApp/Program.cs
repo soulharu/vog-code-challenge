4e168c9b42c4edc35f7482ec2e96910cdfb649f3
Sergio B. Ramos <sbramosj@gmail.com>
2022-01-16 23:57:06 -0300

    Add console app and solved question class

---
+++ VogCodeChallenge.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VogCodeChallenge.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            QuestionClass.IterateThrough();
+        }
+    }
+}
---
+++ VogCodeChallenge.ConsoleApp/QuestionClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VogCodeChallenge.ConsoleApp
+{
+    public static class QuestionClass
+    {
+        static List<string> NamesList = new List<string>()
+        {
+            "Jimmy",
+            "Jeffrey",
+            "John",
+        };
+
+        public static void IterateThrough()
+        {
+            //Method 1
+            var concatenate = string.Join(", ", NamesList);
+            Console.WriteLine($"Iterated with string.Join() to concatenate: {concatenate}. \n");
+
+            //Method 2
+            var linq = NamesList.Where(n => n.EndsWith("y")).ToList();           
+            Console.WriteLine("Iterated with Linq and lambda expression filtering names ending with \"y\": ");
+            linq.ForEach(i => Console.WriteLine($"- {i}"));
+        }
+    }
+}
