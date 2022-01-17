005cbd1aa04723012f98c98ce51a5112ca5c4ab2
Sergio B. Ramos <sbramosj@gmail.com>
2022-01-17 00:47:38 -0300

    Add TestModule method and update program.cs to better show methods being tested

--- VogCodeChallenge.ConsoleApp/Program.cs
+++ VogCodeChallenge.ConsoleApp/Program.cs
@@ -6,7 +6,26 @@ namespace VogCodeChallenge.ConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Step 7: \n");
             QuestionClass.IterateThrough();
+
+            Console.WriteLine("\n\nStep 8: \n");
+            var entryArray = new object[] { 1, 5, 0, 1.0f, 5.34f, "potato", (double)9 };
+
+            foreach (var entry in entryArray)
+            {
+                Console.WriteLine($"Entry: {entry}, Type: {entry.GetType().Name}");
+                
+                try 
+                { 
+                    var result = QuestionClass.TESTModule(entry);
+                    Console.WriteLine($"Result: {result}, Type: {result.GetType().Name} \n");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Exception thrown: {ex.Message} \n");
+                }
+            }
         }
     }
 }
--- VogCodeChallenge.ConsoleApp/QuestionClass.cs
+++ VogCodeChallenge.ConsoleApp/QuestionClass.cs
@@ -24,5 +24,29 @@ namespace VogCodeChallenge.ConsoleApp
             Console.WriteLine("Iterated with Linq and lambda expression filtering names ending with \"y\": ");
             linq.ForEach(i => Console.WriteLine($"- {i}"));
         }
+
+        public static object TESTModule(object entry)
+        {
+            switch (entry)
+            {
+                case int i when (i <= 0):
+                    throw new Exception("Entry value can't be a interger smaller than 1.");
+
+                case int i when (i > 4):
+                    return i * 3;
+
+                case int i:
+                    return i * 2;
+
+                case float f when (f == 1.0f || f == 2.0f):
+                    return 3.0f;
+
+                case string s:
+                    return s.ToUpper();
+
+                default:
+                    return entry;
+            }
+        }
     }
 }
